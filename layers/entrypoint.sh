aed15d6d127bee8fa07bc9f364c98851b576b225
Matti Rantakömi <matti@rantakomi.fi>
2025-03-24 14:49:34 +0200

    improve logging

--- layers/entrypoint.sh
+++ layers/entrypoint.sh
@@ -10,9 +10,9 @@ while true; do
   set +e
     while true; do 
       date
-      echo -n "primary: " ; wget -q -O - https://wss-primary.slack.com | sed -e 's/<[^>]*>//g' ; echo
-      echo -n "backup: " ; wget -q -O - https://wss-backup.slack.com | sed -e 's/<[^>]*>//g' ; echo
-      echo -n "mobile: " ; wget -q -O - https://wss-mobile.slack.com | sed -e 's/<[^>]*>//g' ; echo
+      echo -n "primary: " ; curl --connect-timeout 10 -s -w "\nConnect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total} \n" https://wss-primary.slack.com | sed -e 's/<[^>]*>//g' ; echo
+      echo -n "backup: " ; curl --connect-timeout 10 -s -w "\nConnect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total} \n" https://wss-backup.slack.com | sed -e 's/<[^>]*>//g' ; echo
+      echo -n "mobile: " ; curl --connect-timeout 10 -s -w "\nConnect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total} \n" https://wss-mobile.slack.com | sed -e 's/<[^>]*>//g' ; echo "---"
       sleep 5
     done
   set -e
